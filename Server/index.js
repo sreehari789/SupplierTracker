f114f2d9039a1f61cd93b7f352a8c9862dc7ac2f
sreehari789 <sreeharipraj3693@gmail.com>
2023-05-21 19:11:04 +0530

    first commit

---
+++ Server/index.js
@@ -0,0 +1,44 @@
+const express=require('express')
+const cors=require('cors')
+const dotenv=require('dotenv')
+const morgan=require("morgan")
+const db=require('./config/db')
+const server=express()
+dotenv.config()
+const passport=require('passport')
+const session =require('express-session')
+
+server.use(express.json())
+server.use(cors({
+    origin:'http://localhost:3000'
+}))
+
+server.use(morgan('dev'))
+// database
+db();
+
+// setup port for server app
+const PORT=process.env.PORT
+server.listen(PORT,()=>{
+    console.log(`server started at ${PORT}`);
+})
+
+server.use("/api", require("./routes/user"));
+server.use('/api/vendor', require('./routes/vendor'))
+
+
+// Express session middleware
+server.use(
+    session({
+      secret: '1234',
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+require('./config/passport')
+
+
+// Passport middleware
+server.use(passport.initialize());
+server.use(passport.session());
